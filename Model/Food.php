5ac470461a67ddbeb5a67c0556a6acd44274df50
PatrizioCorcione <patriziocorcione1995@gmail.com>
2024-05-03 15:32:08 +0200

    [end] struttura food

--- Model/Food.php
+++ Model/Food.php
@@ -1,4 +1,18 @@
 <?php
 class Food extends Products
 {
+  public $size;
+  public $taste;
+  public $foodtype;
+
+  function __construct(string $_name, string $_categorie, float $_price, string $_type, string $_img, float $_size, string $_taste, string $_foodtype)
+
+  {
+    parent::__construct($_name,  $_categorie,  $_price,  $_type,  $_img);
+    $this->size = $_size;
+    $this->taste = $_taste;
+    $this->foodtype = $_foodtype;
+  }
+  // tipe matirial age 
+  // matirial size tipe 
 }
--- index.php
+++ index.php
@@ -3,13 +3,17 @@ require_once __DIR__ . '/Model/Products.php';
 require_once __DIR__ . '/Model/Categories.php';
 require_once __DIR__ . '/Model/Food.php';
 
-$trainer = new Food("Croccantini Trainer", "Cani", 12.12, "fdgdgfdfg", 'dsfsdf');
-$kitty = new Food("Croccantini Kitty", "Gatti", 12.12, "fdgdgfdfg", 'dsfsdf');
+$trainer = new Food("Croccantini Trainer", "Cani", 12.12, "fdgdgfdfg", 'dsfsdf', 80.50, 'fragola', 'big');
+$kitty = new Food("Croccantini Kitty", "Gatti", 12.12, "fdgdgfdfg", 'dsfsdf', 80.50, 'fragola', 'big');
+$ciccio = new Food("Croccantini Trainer", "Cani", 12.12, "fdgdgfdfg", 'dsfsdf', 80.50, 'fragola', 'big');
+$pasticcio = new Food("Croccantini Kitty", "Gatti", 12.12, "fdgdgfdfg", 'dsfsdf', 80.50, 'fragola', 'big');
 
 $categoriaCani = new Categories("Cani");
 $categoriaGatti = new Categories("Gatti");
 $categoriaCani->addProduct($trainer);
 $categoriaGatti->addProduct($kitty);
+$categoriaCani->addProduct($ciccio);
+$categoriaGatti->addProduct($pasticcio);
 var_dump($categoriaCani);
 var_dump($categoriaGatti);
 ?>
